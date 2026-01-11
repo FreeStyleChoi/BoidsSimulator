57b89de69d1d69840d3f78514caca15c04b4ea3a
suyoung <suyoung@WIN-9KVD29LSA2I>
2026-01-11 22:41:33 +0900

    가속도, 힘 추가, 이동방향 == 앵글

--- main.cpp
+++ main.cpp
@@ -3,65 +3,124 @@
 #include <vector>
 #include <random>
 
+// 커밋 메세지: 가속도, 힘 추가, 이동방향 == 앵글
+// TODO topX, Y 만들어서 밀고 땡기는게 더 정확하게 만들기
+
 const SDL_Rect windowRect{ 0, 0, 1280, 720 };
 
 class boids
 {
 public:
-	float w;
-	float h;
+	float w = 20.f;
+	float h = 40.f;
 	int count;
-	SDL_FColor defaultColor;
+	SDL_FColor defaultColor = { 1.0f, 0.84f, 0.0f, 1.0f };
 	std::vector<float> midX;
 	std::vector<float> midY;
-	std::vector<float> degree;
+	std::vector<float> angle;
 	std::vector<float> v0;
 	std::vector<float> vx;
 	std::vector<float> vy;
+	std::vector<float> ax;
+	std::vector<float> ay;
 	std::vector<SDL_Vertex> vertices;
-	boids(int boidC, int w = 20, int h = 40, SDL_FColor defaultColor = { 1.0f, 0.84f, 0.0f, 1.0f });
+	std::vector<float> distances;
+	std::vector<float> distancesX;
+	std::vector<float> distancesY;
+	boids(int boidC);
+	float getDistance(float x1, float y1, float x2, float y2);
+	void updateDistances();
+	void updateAcceleration();
 	void updateVectices(float dt);
 	void makeVertices();
 private:
 
 };
 
-boids::boids(int boidC, int w, int h, SDL_FColor defaultColor)
+boids::boids(int boidC)
 {
 	SDL_Vertex temp{};
-	this->w = w;
-	this->h = h;
-	this->count = boidC;
-	this->defaultColor = defaultColor;
+	count = boidC;
 	temp.color = defaultColor;
 	
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_int_distribution<int> genX(this->w, windowRect.w - this->w);
-	std::uniform_int_distribution<int> genY(this->h, windowRect.h - this->h);
-	std::uniform_int_distribution<int> genDegree(0, 359);
+	std::uniform_int_distribution<int> genX(w, windowRect.w - w);
+	std::uniform_int_distribution<int> genY(h, windowRect.h - h);
+	std::uniform_real_distribution<float> genangle(0.f, 2.f*SDL_PI_F);
 	
 	for (int i = 0; i < count; i++)
 	{
 		midX.push_back(genX(gen));
 		midY.push_back(genY(gen));
-		degree.push_back(genDegree(gen));
-		v0.push_back(5.f);
-		vx.push_back(v0[i] * SDL_cosf(degree[i] * (SDL_PI_F / 180.f)));
-		vy.push_back(v0[i] * SDL_sinf(degree[i] * (SDL_PI_F / 180.f)));
+		angle.push_back(genangle(gen));
+		v0.push_back(1.f);
+		vx.push_back(v0[i] * SDL_cosf(angle[i]));
+		vy.push_back(v0[i] * SDL_sinf(angle[i]));
+		ax.push_back(0.f);
+		ay.push_back(0.f);
 		for (int j = 0; j < 3; j++)
 		{
-			this->vertices.push_back(temp);	// init vertices
+			vertices.push_back(temp);	// init vertices
+		}
+	}
+	
+	distances.resize(count * count);
+	distancesX.resize(count * count);
+	distancesY.resize(count * count);
+}
+
+float boids::getDistance(float x1, float y1, float x2, float y2)
+{
+	float dx = x2 - x1;
+	float dy = y2 - y1;
+	float d = SDL_sqrtf(dx * dx + dy * dy);
+	return d;
+}
+
+void boids::updateDistances()
+{
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = 0; j < count; j++)
+		{
+			distances[count * i + j] = getDistance(midX[i], midY[i], midX[j], midY[j]);
+			distancesX[count * i + j] = midX[i] - midX[j];
+			distancesY[count * i + j] = midY[i] - midY[j];
+
+		}
+	}
+	return;
+}
+
+void boids::updateAcceleration()
+{
+	for (int i = 0; i < count; i++)
+	{
+		ax[i] = 0.f;
+		ay[i] = 0.f;
+		for (int j = 0; j < count; j++)
+		{
+			if (i != j)
+			{
+				ax[i] += distancesX[count * i + j] / powf(distances[count * i + j], 3.f);
+				ay[i] += distancesY[count * i + j] / powf(distances[count * i + j], 3.f);
+
+			}
 		}
 	}
 }
 
 void boids::updateVectices(float dt)
 {
-	for (int i = 0; i < this->count; i++)
+	for (int i = 0; i < count; i++)
 	{
-		this->midX[i] += vx[i] * dt;
-		this->midY[i] += vy[i] * dt;
+		vx[i] += ax[i] * dt;
+		vy[i] += ay[i] * dt;
+		midX[i] += vx[i] * dt;
+		midY[i] += vy[i] * dt;
+		
+		angle[i] = SDL_atan2f(vy[i], vx[i]);
 
 		if (midX[i] > windowRect.w)
 		{
@@ -87,21 +146,21 @@ void boids::updateVectices(float dt)
 void boids::makeVertices()
 {
 
-	for (int i = 0; i < this->count; i++)
+	for (int i = 0; i < count; i++)
 	{
-		float theta = degree[i] * (SDL_PI_F / 180.f);
+		float theta = angle[i];
 
-		this->vertices[3*i].position.x = this->midX[i] + this->h * SDL_cosf(theta);							// P1x
-		this->vertices[3*i].position.y = this->midY[i] + this->h * SDL_sinf(theta);							// P1y
-		vertices[3 * i].color = { 1.0, 0.0, 0.0, 1.0 };
+		vertices[3*i].position.x = midX[i] + h * SDL_cosf(theta);									// P1x
+		vertices[3*i].position.y = midY[i] + h * SDL_sinf(theta);									// P1y
+		//vertices[3 * i].color = { 1.0, 0.0, 0.0, 1.0 };		// It sets boids rainbow
+		
+		vertices[3*i+1].position.x = midX[i] + w / 2 * SDL_cosf(theta + (SDL_PI_F / 2.f));			// P2x
+		vertices[3*i+1].position.y = midY[i] + w / 2 * SDL_sinf(theta + (SDL_PI_F / 2.f));			// P2y
+		//vertices[3 * i + 1].color = { 0.0, 1.0, 0.0, 1.0 };	// It sets boids rainbow
 
-		this->vertices[3*i+1].position.x = this->midX[i] + this->w / 2 * SDL_cosf(theta + (SDL_PI_F / 2.f));	// P2x
-		this->vertices[3*i+1].position.y = this->midY[i] + this->w / 2 * SDL_sinf(theta + (SDL_PI_F / 2.f));	// P2y
-		vertices[3 * i + 1].color = { 0.0, 1.0, 0.0, 1.0 };
-
-		this->vertices[3*i+2].position.x = this->midX[i] + this->w / 2 * SDL_cosf(theta + (SDL_PI_F / 2.f * 3.f));	// P3x
-		this->vertices[3*i+2].position.y = this->midY[i] + this->w / 2 * SDL_sinf(theta + (SDL_PI_F / 2.f * 3.f));	// p3y
-		vertices[3 * i + 2].color = { 0.0, 0.0, 1.0, 1.0 };
+		vertices[3*i+2].position.x = midX[i] + w / 2 * SDL_cosf(theta + (SDL_PI_F / 2.f * 3.f));		// P3x
+		vertices[3*i+2].position.y = midY[i] + w / 2 * SDL_sinf(theta + (SDL_PI_F / 2.f * 3.f));		// p3y
+		//vertices[3 * i + 2].color = { 0.0, 0.0, 1.0, 1.0 };	// It sets boids rainbow
 	}
 
 	return;
@@ -111,7 +170,7 @@ void boids::makeVertices()
 int main(int argc, char** argv)
 {
 	float dt = 0.1;
-	boids Boids(10);
+	boids Boids(50);
 	SDL_Window* window = NULL;
 	SDL_Renderer* renderer = NULL;
 	SDL_Event event;
@@ -149,6 +208,8 @@ int main(int argc, char** argv)
 			isRunning = false;
 		}
 
+		Boids.updateDistances();
+		Boids.updateAcceleration();
 		Boids.updateVectices(dt);
 		Boids.makeVertices();
 
